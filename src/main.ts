4d472592f6182fed3926fa1fb81296a360aaa009
Kayeer <boloto.hub@gmail.com>
2025-06-04 15:00:10 +0300

    chore: main config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import EnvConfig from './config/envConfig';
 
-async function bootstrap() {
+async function bootstrap(port: number) {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(EnvConfig.app.port);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(port, () =>
+    // eslint-disable-next-line no-console
+    console.log(`Server start on port: ${port}`),
+  );
 }
-bootstrap();
+
+bootstrap(EnvConfig.app.port);
